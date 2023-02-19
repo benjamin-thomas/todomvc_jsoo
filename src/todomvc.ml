dc42afd6feb4eed5ce04ddf7c5be8fdc0272132d
Benjamin <benjamin.guy.thomas@gmail.com>
2023-02-19 07:58:57 +0100

    Basic list display works

--- src/todomvc.ml
+++ src/todomvc.ml
@@ -12,29 +12,26 @@ let task_input =
 let task_input_dom = Js_of_ocaml_tyxml.Tyxml_js.To_dom.of_input task_input
 let set_task_input v = task_input_dom##.value := Js_of_ocaml.Js.string v
 
-(*
-let task_entry ((r, f) : rp) =
-  bind_event Ev.keypresses task_input_dom (fun evt ->
-      Lwt.return
-      @@
-      if evt##.keyCode = 13 then (
-        Controller.update Add (r, f)
-        ; set_task_input ""
-      ))
-
-  ; bind_event Ev.inputs task_input_dom (fun _ ->
-        Lwt.return
-        @@ Controller.update (Update_field task_input_dom##.value) (r, f))
-
-  ; Html5.(
-      header ~a:[ a_class [ "header" ] ] [ h1 [ txt "todos" ]; task_input ])
-;;
-*)
-
-type task =
-  { description : string
-  ; id : int
+type todo =
+  { descr : string
   }
+
+type model =
+  { todos : todo list
+  }
+
+let init =
+  { todos = [{descr = "First todo!"}
+            ; {descr = "Second todo!"}]}
+
+(* rs => reactive signal? *)
+type rs = model React.signal
+
+(* rf => reactive function? *)
+type rf = ?step:React.step -> model -> unit
+
+(* rp => reactive pair? *)
+type rp = rs * rf
 
 type action = Add
 
@@ -54,14 +51,58 @@ let task_entry (r, f) =
          print_endline "Some other key was pressed!";
        Lwt.return ())
 
-let view =
+
+
+let css_visibility (model:model) =
+  match model.todos with
+  | [] -> "visibility: hidden;"
+  | _ -> "visibility: visible;"
+
+let todo_item ((r, f) : rp) (todo : todo) =
+  Js_of_ocaml_tyxml.Tyxml_js.Html5.(
+    li
+      [ div
+          ~a:[ a_class [ "view" ] ]
+          [txt todo.descr]
+      ])
+
+let todo_list ((r, f) : rp) =
+  let css_visibility (todos: todo list) =
+    match todos with
+    | [] -> "visibility: hidden;"
+    | _ -> "visibility: visible;"
+  in
+  let list_of_visible_tasks m =
+    let is_visible _todo = true in
+    List.filter is_visible m.todos
+  in
+
+  let r_todos = React.S.map (fun m -> m.todos) r in
+  let rl =
+    ReactiveData.RList.from_signal (React.S.map list_of_visible_tasks r)
+  in
+  let rl = ReactiveData.RList.map (todo_item (r, f)) rl in
+  Js_of_ocaml_tyxml.Tyxml_js.(
+    Html5.(
+      section
+        ~a:
+          [ a_class [ "main" ]
+          ; R.Html5.a_style (React.S.map css_visibility r_todos)
+          ]
+        [ Js_of_ocaml_tyxml.Tyxml_js.Html5.input ()
+        ; label ~a:[ a_label_for "toggle-all" ] [ txt "Mark all as complete" ]
+        ; R.Html5.ul ~a:[ a_class [ "todo-list" ] ] rl
+        ]))
+;;
+
+let view ((r, f):rp) =
   Js_of_ocaml_tyxml.Tyxml_js.Html5.(
     div
       ~a:[ a_class [ "todomvc-wrapper" ] ]
       [ section ~a:[ a_class [ "todoapp" ] ]
           [ header ~a:[a_class ["header"]]
               [ h1 [txt "todos"]
-              ; task_input
+              ; todo_list (r, f)
               ]
           ]
       ; footer ~a: [a_class ["info"]]
@@ -72,32 +113,14 @@ let view =
       ])
 ;;
 
-(*
- Dom.appendChild parent
-    (Js_of_ocaml_tyxml.Tyxml_js.To_dom.of_div (View.view rp))
-*)
 
 let main (_ : Js_of_ocaml.Dom_html.event Js_of_ocaml.Js.t) =
+  let (rp:rp) = React.S.create init in
   let document = Js_of_ocaml.Dom_html.document in
-  (* let child = Js_of_ocaml.Dom_html.createH1 document in
-     Js_of_ocaml.Dom.appendChild document##.body child
-     ; child##.textContent :=
-         Js_of_ocaml.Js.Opt.return (Js_of_ocaml.Js.string "Hello from JSOO!"); *)
   Js_of_ocaml.Dom.appendChild document##.body
-    (Js_of_ocaml_tyxml.Tyxml_js.To_dom.of_div view)
+    (Js_of_ocaml_tyxml.Tyxml_js.To_dom.of_div @@ view rp)
 ; Lwt.return ()
 ;;
-
-(*
-let _ = Lwt.bind (Js_of_ocaml_lwt.Lwt_js_events.onload ()) (fun evt -> main evt)
-*)
-
-(*
-let _ =
-  let open Lwt.Infix in
-  Js_of_ocaml_lwt.Lwt_js_events.onload () >>= main
-;;
-*)
 
 let _ =
   let%lwt evt = Js_of_ocaml_lwt.Lwt_js_events.onload () in
